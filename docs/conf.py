3c6cda00a86d355cdf6dbf736ece22b0d6520cd0
Ростислав Жуков <rosf.zhukov@gmail.com>
2024-02-01 15:56:22 +0300

    Docs build fixes

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../tests/'))
 
 project = 'Python Algorithms and Datastructures'
 copyright = '2023, zhukovrost'
